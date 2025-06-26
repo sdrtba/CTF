2ab895a145db21f03a15eb0191ab350b76cc7103
sdrtba <sdrtba@yandex.ru>
2025-06-27 00:57:49 +0400

    update nifnif

--- 1.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-with open("1.txt", "r") as file:
-    lines = file.readlines()
-
-for line in lines:
-    line = base64.b64decode(line.encode("utf-8")).decode("utf-8").replace("*", "")
-    if "flag" in line:
-        print(line)
